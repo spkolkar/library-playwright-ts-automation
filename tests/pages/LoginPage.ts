16a2dbb2be1c24d3e3760303b33d61a1f7cd4aed
shivakumar kolkar <shivakumar.kolkar@gmail.com>
2026-02-14 16:25:07 +0000

    add few more files

---
+++ tests/pages/LoginPage.ts
@@ -0,0 +1,23 @@
+import { Page, Locator } from "@playwright/test";  
+import { BasePage } from "./BasePage";
+
+export class LoginPage extends BasePage {
+  readonly usernameInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.usernameInput = page.getByRole('textbox', { name: 'Enter your username' });
+    this.passwordInput = page.getByRole('textbox', { name: 'Enter your password' });
+    this.loginButton = page.getByRole('button', { name: 'Submit login' });
+  }
+  async goto() {
+    await this.page.goto('/');
+  } 
+  async login(username: string, password:string) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
--- tests/specs/login.spec.ts
+++ tests/specs/login.spec.ts
@@ -1,36 +1,55 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
 
-test('Login Test', async ({ page }) => {
-  await page.goto('https://frontendui-librarysystem.onrender.com/');
-  await page.getByRole('button', { name: 'Start Testing' }).click();
-  await page.goto('https://frontendui-librarysystem.onrender.com/login');
 
-  await page.getByTestId('login-heading').isVisible();
+test.describe('Library System Tests', () => {
+  test.beforeAll(async ({ page }) => {
+    await page.goto('https://frontendui-librarysystem.onrender.com/');
+    await page.getByRole('button', { name: 'Start Testing' }).click();
+  });
 
-  await page.getByRole('textbox', { name: 'Enter your username' }).click();
-  await page.getByRole('textbox', { name: 'Enter your username' }).fill('admin');
-  await page.getByRole('textbox', { name: 'Enter your password' }).fill('admin');
-  await page.getByRole('button', { name: 'Submit login' }).click();
+  
+test('Valid Login Test', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  const username = process.env.USERNAME;
+  const password = process.env.PASSWORD;
+  if (!username || !password) {
+    throw new Error('Missing USERNAME or PASSWORD in environment variables');
+  }
+  await loginPage.login(username, password);
+  await expect(page).toHaveURL('books');
+  // await page.goto('https://frontendui-librarysystem.onrender.com/');
+  // await page.getByRole('button', { name: 'Start Testing' }).click();
+  // await page.goto('https://frontendui-librarysystem.onrender.com/login');
+
+  // await page.getByTestId('login-heading').isVisible();
+
+  // await page.getByRole('textbox', { name: 'Enter your username' }).click();
+  // await page.getByRole('textbox', { name: 'Enter your username' }).fill('admin');
+  // await page.getByRole('textbox', { name: 'Enter your password' }).fill('admin');
+  // await page.getByRole('button', { name: 'Submit login' }).click();
 
 });
+  
+});
+
+
 
 
 test('Add Book Test', async ({ page }) => {
 
-    // await page.getByRole('button', { name: 'Start Testing' }).click();
+   
   await page.goto('https://frontendui-librarysystem.onrender.com/books');
   await page.getByRole('button', { name: 'Add Book' }).click();
-  // await page.getByRole('textbox', { name: 'Title:' }).click();
+  
   await page.getByRole('textbox', { name: 'Title:' }).fill('mybook');
-  // await page.getByRole('textbox', { name: 'Author:' }).click();
+
   await page.getByRole('textbox', { name: 'Author:' }).fill('tester');
   await page.getByLabel('Genre:').selectOption('Fiction');
-  // await page.getByRole('textbox', { name: 'ISBN:' }).click();
+ 
   await page.getByRole('textbox', { name: 'ISBN:' }).fill('tgyty');
   await page.getByRole('textbox', { name: 'Publication Date:' }).fill('2026-02-13');
-  // await page.getByRole('textbox', { name: 'Publication Date:' }).press('Tab');
-  // await page.getByRole('textbox', { name: 'Publication Date:' }).press('Tab');
-  // await page.getByRole('textbox', { name: 'Price:' }).click();
   await page.getByRole('textbox', { name: 'Price:' }).fill('10');
   await page.getByRole('button', { name: 'Submit Add New Book Form' }).click();
   // await page.getByRole('button', { name: 'Edit' }).nth(3).click();
