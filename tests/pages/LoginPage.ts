65fd23af3157b1a388550afbc8fcf8913be96006
shivakumar kolkar <shivakumar.kolkar@gmail.com>
2026-02-17 19:56:42 +0000

    added readme file

--- tests/pages/LoginPage.ts
+++ tests/pages/LoginPage.ts
@@ -15,24 +15,16 @@ export class LoginPage {
 constructor(page: Page) {
     this.page = page;
 
-    // Header (role-based)
+    // Header
     this.loginHeader = page.getByRole('heading', { name: 'Login' });
 
-    // Labels (strict by "for" attribute)
+    // Labels
     this.usernameLabel = page.locator('label[for="username"]');
     this.passwordLabel = page.locator('label[for="password"]');
 
     // Button
     this.loginButton = page.getByRole('button', { name: 'Login' });
   }
-  // constructor(page: Page) {
-  //   this.page = page;
-
-  // this.usernameLabel = page.getByRole('heading', { name: 'Username' });
-  // this.passwordLabel = page.getByRole('heading', { name: 'Password' });
-  // this.loginButton = page.getByRole('button', { name: 'Submit login' });
-  // this.loginHeader = page.getByRole('heading', { name: /Login/i });
-  // }
 
   public async goto(): Promise<void> {
     const baseUrl = process.env.BASE_URL;
@@ -49,31 +41,14 @@ constructor(page: Page) {
   public async validateLoginHeader(expectedText: string): Promise<void> {
     await expect(this.loginHeader).toHaveText(expectedText);
   }
-  // public async validateLabelText(
-  //   locator: Locator,
-  //   expectedText: string
-  // ): Promise<void> {
-  //   await expect(locator).toHaveText(expectedText);
-  // }
-
-public async validateUsernameLabel(expectedText: string): Promise<void> {
+  
+  public async validateUsernameLabel(expectedText: string): Promise<void> {
     await expect(this.usernameLabel).toHaveText(expectedText);
   }
 
   public async validatePasswordLabel(expectedText: string): Promise<void> {
     await expect(this.passwordLabel).toHaveText(expectedText);
   }
-
-  // public async validateLoginHeader(expectedText: string): Promise<void> {
-  //   await this.validateLabelText(this.loginHeader, expectedText);
-  // }
-
-  // public async validateLoginPageLoaded(): Promise<void> {
-  //   await expect(this.loginHeader).toHaveText(expectedText);
-  //   await expect(this.usernameLabel).toBeVisible();
-  //   await expect(this.passwordLabel).toBeVisible();
-  //   await expect(this.loginButton).toBeVisible();
-  // }
 
   public async validateLoginButtonName(expectedText: string): Promise<void> {
     await expect(this.loginButton).toHaveText(expectedText);
@@ -91,34 +66,3 @@ public async validateUsernameLabel(expectedText: string): Promise<void> {
     await this.loginButton.click();
   }
 }
-
-// export class LoginPage extends BasePage {
-//   public usernameInput: Locator;
-//   public passwordInput: Locator;
-//   public loginButton: Locator;
-
-//   constructor(page: Page) {
-//     super(page);
-//     this.usernameInput = this.page.getByRole('textbox', { name: 'Enter your username' });
-//     this.passwordInput = this.page.getByRole('textbox', { name: 'Enter your password' });
-//     this.loginButton = this.page.getByRole('button', { name: 'Submit login' });
-//   }
-// /**
-//  * 
-//  * @param url : pass the url to which user wants to navigate
-//  */
-//   public async goto(url: string) {
-//     await this.page.goto(url);
-//   }
-
-//   // public async login(username: string, password: string) {
-  
-//   // }
-
-//   public async validateLoginHeaderDisplayed(loginHeader: string) {
-//     const loginHeaderElement = await this.getDisplayedElementBySelector(LOGIN_HEADER);
-//     log('This is the displayed login header ${loginHeaderElement}',LogType.INFO);
-//     await expect(loginHeaderElement).toBeEqual(loginHeader);
-//   }
-  
-// }
--- tests/specs/login.spec.ts
+++ tests/specs/login.spec.ts
@@ -5,69 +5,23 @@ import { LoginPageSteps } from '../steps/loginPage-steps';
 //  
 
 test.describe('Login Page Tests', () => {
-  test('Validate login page displayed labels ', async ({ page }) => {
+  test('Validate login page UI  Elements ', async ({ page }) => {
     const loginSteps = new LoginPageSteps(page);
 
     await loginSteps.navigateToLoginPage();
-    await loginSteps.validateLoginLabels();
+    await loginSteps.validateLoginPageUI();
   });
 
-  // test('Admin user login test', async ({ page }) => {
-  //   const loginSteps = new LoginPageSteps(page);
+  test('Admin user login test', async ({ page }) => {
+    const loginSteps = new LoginPageSteps(page);
 
-  //   await loginSteps.navigateToLoginPage();
-  //   await loginSteps.loginWithCredentials(username!, password!
-  //   );
-  // });
+    await loginSteps.navigateToLoginPage();
+    await loginSteps.loginWithCredentials(username!, password!
+    );
+  });
 });
 
-// test.describe('Library System Tests', () => {
-  
-  
-// test('Valid Login Test', async ({ page }) => {
-//   const loginPage = new LoginPage(page);
-//   const baseUrl = process.env.BASE_URL;
-//   const username = process.env.USER_NAME;
-//   const password = process.env.PASSWORD;
-//   // console.log("Name",username);
-//   // console.log("password",password);
-
-//   if (!baseUrl || !username || !password) {
-//     throw new Error('Missing BASE_URL, USERNAME or PASSWORD in environment variables');
-//   }
-
-//   const loginUrl = `${baseUrl.replace(/\/$/, '')}/login`;
-//   console.log(`Navigating to ${loginUrl}`);
-//   await loginPage.goto(loginUrl);
-
-//   await expect(loginPage.usernameInput).toBeVisible();
-//   await expect(loginPage.passwordInput).toBeVisible();
-//   await expect(loginPage.loginButton).toBeVisible();
- 
-//   await loginPage.login(username, password);
-
-//   // after login we should no longer be on the /login page
-//   // await expect(page).not.toHaveURL(/\/login$/);
- 
-// });
-  
-// });
-
-
-
-
-// test('Add Book Test', async ({ page }) => {
-
-   
-//   await page.goto('https://frontendui-librarysystem.onrender.com/books');
-//   await page.getByRole('button', { name: 'Add Book' }).click();
-  
-//   await page.getByRole('textbox', { name: 'Title:' }).fill('mybook');
-
-//   await page.getByRole('textbox', { name: 'Author:' }).fill('tester');
-//   await page.getByLabel('Genre:').selectOption('Fiction');
- 
-//   await page.getByRole('textbox', { name: 'ISBN:' }).fill('tgyty');
+// t//   await page.getByRole('textbox', { name: 'ISBN:' }).fill('tgyty');
 //   await page.getByRole('textbox', { name: 'Publication Date:' }).fill('2026-02-13');
 //   await page.getByRole('textbox', { name: 'Price:' }).fill('10');
 //   await page.getByRole('button', { name: 'Submit Add New Book Form' }).click();
